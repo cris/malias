0f210804b2615aef4c614cc668da31ec4c69e350
Sergii Boiko <cris.kiev@gmail.com>
2014-05-02 00:28:42 +0300

    Test fun module/function substitution

--- test/malias_tests.erl
+++ test/malias_tests.erl
@@ -9,6 +9,11 @@ lists_import_test() ->
     Seq = l:seq(1, 5),
     ?assertEqual([1,2,3,4,5], Seq).
 
+fun_import_test() ->
+    Fun = fun l:seq/2,
+    Seq = Fun(1, 5),
+    ?assertEqual([1,2,3,4,5], Seq).
+
 string_import_test() ->
     Len = s:len("12345"),
     ?assertEqual(5, Len).
